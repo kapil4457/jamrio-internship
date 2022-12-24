14ac6a27449cda78d5545457530ad5d7e9aa6ba5
Kapil Soni <kapilsoni54768161@gmail.com>
2022-12-24 21:19:37 +0530

    Done with the description page

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -14,7 +14,6 @@ const Description = () => {
         "app-id": "63a6db18ce8dda3753ade844",
       },
     };
-    console.log("In");
     const { data } = await axios.get(
       `https://dummyapi.io/data/v1/post/${params.id}`,
       config
@@ -37,6 +36,42 @@ const Description = () => {
       ) : (
         <div className="desc-info">
           <img src={posts?.image} alt="" />
+          <div className="info-main">
+            <div className="source info">
+              <b>Source </b>
+              <a href={posts.link} target="__blank" className="source">
+                Link
+              </a>
+            </div>
+            <div className="likes info">
+              <b>Likes </b>
+              {posts.likes}
+            </div>
+            <div className="tags info">
+              <b>Tags </b>
+              <div>
+                {posts.tags.map((tag, key) => (
+                  <span key={key}>{tag}</span>
+                ))}
+              </div>
+            </div>
+            <div className="publish-date info">
+              <b>Published On </b>
+              {posts.publishDate.substr(0, 10)}
+            </div>
+            <div className="description info">
+              <b>Description </b>
+              {posts.text}
+            </div>
+            <div className="owner info">
+              <b>Owner </b>
+              {posts.owner.title +
+                ". " +
+                posts.owner.firstName +
+                " " +
+                posts.owner.lastName}
+            </div>
+          </div>
         </div>
       )}
     </div>
